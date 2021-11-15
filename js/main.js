aeee28c1d400cd04a68eab5a8350f897d56b05a5
marcusepaula <93179224+marcusepaula@users.noreply.github.com>
2021-11-15 00:04:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
   
 /* 1. Proloder */
     $(window).on('load', function () {
-      $('#Preloader').delay(450).fadeOut('slow');
+      $('.Preloader').delay(450).fadeOut('slow');
       $('body').delay(450).css({
         'overflow': 'visible'
       });
